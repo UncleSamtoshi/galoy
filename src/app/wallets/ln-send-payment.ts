ddbd7fe1f69a9cfd8524c1514bd1f2f4d386fba8
vindard <17693119+vindard@users.noreply.github.com>
2021-11-03 15:31:09 -0400

    Pass 'paymentId' property in at app layer to fee-reimburse methods

--- src/app/wallets/ln-send-payment.ts
+++ src/app/wallets/ln-send-payment.ts
@@ -557,6 +557,7 @@ const executePaymentViaLn = async ({
       const reimbursed = await reimburseFee({
         liabilitiesAccountId,
         journalId,
+        paymentId: persistedPayment.id,
         paymentHash,
         maxFee,
         actualFee: payResult.roundedUpFee,
