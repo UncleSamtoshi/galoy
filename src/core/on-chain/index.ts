ceff45260ec9caff550903f4e804b9fdfcb3ed24
Juan P Lopez <dolcalmi@gmail.com>
2021-12-08 16:58:01 -0500

    Add get balance to on-chain service

--- src/core/on-chain/index.ts
+++ src/core/on-chain/index.ts
@@ -1,5 +1,5 @@
 import assert from "assert"
-import { getChainBalance, sendToChainAddress } from "lightning"
+import { sendToChainAddress } from "lightning"
 
 import { getActiveOnchainLnd } from "@services/lnd/utils"
 import { ledger } from "@services/mongodb"
@@ -239,7 +239,23 @@ export const OnChainMixin = (superclass) =>
 
         const { lnd } = getActiveOnchainLnd()
 
-        const { chain_balance: onChainBalance } = await getChainBalance({ lnd })
+        const getOnChainBalance = async (): Promise<Satoshis> => {
+          const onChainService = OnChainService(TxDecoder(BTC_NETWORK))
+          if (onChainService instanceof Error) {
+            onchainLogger.fatal({ err: onChainService })
+            return toSats(0)
+          }
+
+          const onChainBalance = await onChainService.getBalance()
+          if (onChainBalance instanceof Error) {
+            onchainLogger.fatal({ err: onChainBalance })
+            return toSats(0)
+          }
+
+          return onChainBalance
+        }
+
+        const onChainBalance = await getOnChainBalance()
 
         let id, amountToSend
 
